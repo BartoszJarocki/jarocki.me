d237c4d508eb7078f25a11a42ac45c32730b8154
Bartosz Jarocki <jarocki.bartek@gmail.com>
2020-09-26 14:57:57 +0200

    Og images generation fix

--- src/lib/_og/template.js
+++ src/lib/_og/template.js
@@ -6,7 +6,7 @@ export function getHtml({ title }) {
         <title>Generated Image</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
-            @import url("https://fonts.googleapis.com/css2?family=Inter:wght@700;800:500&display=swap");
+            @import url("https://fonts.googleapis.com/css2?family=Inter:wght@800&display=swap");
             
             html,
             body {
@@ -22,12 +22,12 @@ export function getHtml({ title }) {
             
             .top-background {
                 background: linear-gradient(0deg, rgba(255, 255, 255, 1) 10%, rgba(255, 255, 255, 0) 85%),
-                    url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23bdbdbd' fill-opacity='0.2' fill-rule='evenodd'/%3E%3C/svg%3E");
+                    url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23bdbdbd' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
             }
             
             .main {
               text-align: center;
-              font-family: "Inter", sans-serif;
+
             
               width: 100%;
               height: 100%;
@@ -43,6 +43,8 @@ export function getHtml({ title }) {
               font-size: 5rem;
               word-wrap: break-word;
               letter-spacing: -0.05em;
+              font-weight: 800;
+              font-family: "Inter", sans-serif;
             }
         </style>
         <body>
