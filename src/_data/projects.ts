99a9f5584a810b8d0ed7fe9d872878b48ee5056a
Bartosz Jarocki <jarocki.bartek@gmail.com>
2020-10-06 07:34:23 +0200

    Added jarocki.me as a project

--- src/_data/projects.ts
+++ src/_data/projects.ts
@@ -1,15 +1,21 @@
 export const Projects = [
   {
     title: 'Barepapers',
-    techStack: ['Next.js', 'Puppeteer', '▲Vercel '],
+    techStack: ['Next.js', 'Puppeteer', '▲Vercel'],
     description: 'Minimalist wallpapers generator.',
     link: 'https://barepapers.com',
   },
   {
     title: 'Year progress',
-    techStack: ['Next.js', '▲Vercel '],
+    techStack: ['Next.js', '▲Vercel'],
     description:
       'Shows year progress. 15 seconds before New Year it also shows the fullscreen countdown.',
     link: 'https://getyearprogress.com',
+  },
+  {
+    title: 'Jarocki.me',
+    techStack: ['Next.js', 'Puppeteer', '▲Vercel'],
+    description: 'This blog, open sourced.',
+    link: 'https://github.com/BartoszJarocki/web-jarocki-me',
   },
 ];
