25db66b1add2d94b1153fed00b8f4315320bf873
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-04-17 09:48:12 +0200

    Updated styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,7 +9,7 @@ export const Button = React.forwardRef<HTMLButtonElement, Props>(
     return (
       <button
         ref={ref}
-        className="px-4 py-2 font-medium text-sm leading-5 rounded-md text-gray-500 hover:text-gray-700 hover:outline-none hover:text-gray-700 bg-gray-100 hover:bg-gray-300"
+        className="px-4 py-2 font-medium text-sm leading-5 rounded-md text-gray-500 hover:text-gray-700 hover:outline-none bg-gray-100 hover:bg-gray-300 duration-200 ease-in-out"
         {...otherProps}
       >
         {children}
