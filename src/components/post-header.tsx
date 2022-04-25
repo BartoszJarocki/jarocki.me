554620ce40a20feba03cbda5e3e3196f68bef6d2
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-04-25 20:00:59 +0200

    Mobile padding fix

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -20,7 +20,7 @@ export const PostHeader: React.FC<Props> = ({ title, date, author, readingTime, 
   const router = useRouter();
 
   return (
-    <div className="container mx-auto max-w-2xl flex flex-col flex-grow justify-center overflow-x-hidden">
+    <div className="container mx-auto max-w-2xl px-6 sm:px-0 flex flex-col flex-grow justify-center overflow-x-hidden">
       <div className="flex font-mono text-sm text-gray-800 flex-wrap gap-1">
         {tags.map((tag) => (
           <Badge
