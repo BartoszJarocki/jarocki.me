921e2efd543e0b12745df56af40096f208c0ca4e
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-04-25 19:35:33 +0200

    Mobile padding fix

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Container: React.FC = ({ children }) => {
-  return <main className="container mx-auto pb-6 max-w-2xl overflow-x-hidden">{children}</main>;
+  return (
+    <main className="container mx-auto px-6 sm:px-0 pb-6 max-w-2xl overflow-x-hidden">
+      {children}
+    </main>
+  );
 };
