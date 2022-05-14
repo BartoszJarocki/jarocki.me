54355d03b8e242d713697c17d5b4259b37fc03a8
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-05-14 10:50:33 +0200

    Design improvements

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const PageTitle: React.FC = ({ children }) => {
-  return <h1 className="text-3xl font-bold leading-none tracking-tight">{children}</h1>;
+  return <h1 className="text-4xl font-bold leading-none tracking-tight">{children}</h1>;
 };
