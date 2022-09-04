5a857ddc102809e2a0621e620c4717251284514e
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-09-05 00:10:27 +0200

    Updated deps

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,9 @@ import { withContentlayer } from 'next-contentlayer';
 export default withContentlayer({
   swcMinify: true,
   reactStrictMode: true,
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+    images: { allowFutureImage: true },
+  },
 });
